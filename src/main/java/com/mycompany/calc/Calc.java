010a220296a6167fe7673ffd07ddd9f6a8152429
VED PRATAP SINGH <119805462+Vedprtpsingh@users.noreply.github.com>
2024-06-29 17:10:39 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/calc/Calc.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calc;
+import java.util.*;
+
+/**
+ *
+ * @author @vedprtpsingh
+ */
+public class Calc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
